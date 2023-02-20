ad8a6f51f3e3ba5afef6c9c450b18f6cfeb5ddf9
Sumire Honda <pu.sumirehonda@gmail.com>
2023-02-20 13:49:17 +0000

    cleaned and added speaker tag

--- preprocess.py
+++ preprocess.py
@@ -9,34 +9,68 @@ import random
 max_length = 128 # Should be modified considering bsd's max input size is 278 (en) and 110 (en) , but ami's max input size is 662 (en) and 302 (ja)
 # 520 # 128
 
-def preprocess_function(src_context_size, tgt_context_size, cw_dropout_rate, tgt_sep, tokenizer, data): # data should be splitted into train / dev / test internally
-    inputs = []
-    targets = []
+def preprocess_function(src_lang, tgt_lang, speaker, src_context_size, tgt_context_size, random_context, cw_dropout_rate, tgt_sep, tokenizer, data): 
     new_inputs = []
     new_targets = []
     new_tgt_contexts = []
 
-    for doc in data["conversation"]:
-        doc_input = [sent['en_sentence'] for sent in doc] # Iterate over every documents #list[doc[sent]] # for doc in docs. 
-        doc_target = [sent['ja_sentence'] for sent in doc]
-        inputs.append(doc_input)
-        targets.append(doc_target)
+    """
+    if speaker: 
+        for doc in data["conversation"]:
+            src_speakers =  [sent[f'{src_lang}_speaker'] for sent in doc] 
+            speaker_ids = []
+        
+            for idx, speaker in enumerate(src_speakers):
+                speaker_ids.append(idx)
+            
+            print ("speaker_ids:", speaker_ids)
+        max_speaker_ids = max([max(id) for doc in speaker_ids for id in doc])
+        print ("max_speaker_ids", max_speaker_ids)
 
+        for i in range(max_speaker_ids+1):
+            tokenizer.add_special_tokens({f"speaker_token_{i}":"<S{i}>"})
+        model.resize_token_embeddings(len(tokenizer))
+    """
+
+    # Iterate over every documents
+    for doc in data["conversation"]:
+        doc_input = [sent['en_sentence'] for sent in doc] 
+        doc_target = [sent['ja_sentence'] for sent in doc]
+
+        if speaker:
+            src_speakers =  [sent[f'{src_lang}_speaker'] for sent in doc] 
+            speaker_ids = []
+        
+            for idx, speaker in enumerate(src_speakers):
+                speaker_ids.append(idx)
+            print ("speaker_ids:", speaker_ids)
+       
+        # Concatenate contexts given any context_size both in src and tgt
+        # Source side
         new_doc_input = []
+
         for idx, ip in enumerate (doc_input):
-            src_context_size = random.randint(0, src_context_size) ####################Deal with when the context size is zero ###############
+            if speaker:
+                #speaker_token = f"<S{speaker_ids[idx]}>"
+                #print (speaker_token)
+                #print ("speaker_token", speaker_token) 
+                #ip = f"{tokenizer.speaker_token_}".join([src_speakers[idx], ip])
+                #ip = speaker_token.join([src_speakers[idx], ip])
+                ip = ": ".join([src_speakers[idx], ip])
+                print ("concat_speaker", ip)
+
+            if random_context:
+                src_context_size = random.randint(0, src_context_size) 
             if src_context_size == 0:
                 new_doc_input.append(ip)
     
-        # Context_aware inputs, "new_inputs"
             else:    
-            # Check each inputs 
                 context_list = []  
                 # Check each context index given the context size and current input index
                 for context_window in range(src_context_size, 0, -1):
                     context_idx = idx - context_window
                     
-                    # If context idx is not the left side of the beggining of the inputs
+                    # If context idx is not the left side of the beggining of the doc_inputs
                     if context_idx >= 0:
                         #Store the context in a list
                         context_list.append(doc_input[context_idx])
@@ -46,92 +80,98 @@ def preprocess_function(src_context_size, tgt_context_size, cw_dropout_rate, tgt
                     
                 else:
                     concat_contexts = "</t>".join(context_list)
-                    #print (concat_contexts)
-
                     new_input = "</t>".join([concat_contexts,ip])
-                    #print ("new_input", new_input)
                     new_doc_input.append(new_input)
-                    #print ("new_doc_input", new_doc_input) 
+            
         new_inputs.append(new_doc_input)
-        #print ("-------")
-        #print ("new_inputs_per doc-level:", new_inputs)
-        #print ("-------")
-        #print("new_inputs1", new_inputs)
 
-        # Concatenate contexts given any context_size
+        # Target side
         new_doc_target = []
         # Separate context and current sentences per doc
-        new_doc_context = [] #
+        new_doc_context = [] 
+
         for idx, tgt in enumerate (doc_target):
-            tgt_context_size = random.randint(0, tgt_context_size) ############## Deal with when the context size is zero ############3
-            if tgt_context_size == 0:
+            
+            """
+            if speaker: 
+                tgt = ": ".join([tgt_speakers[idx], tgt])
+                print ("concat_speaker", tgt)
+            """
+
+            target_context_size = tgt_context_size
+
+            if random_context:
+                target_context_size = random.randint(0, tgt_context_size)
+            
+            if target_context_size == 0:
                 new_doc_target.append(tgt) 
-                new_doc_context.append("</t>") # to make context_ids the same shape with input_ids 
+                new_doc_context.append("</t>") # SU: to make context_ids the same shape with input_ids 
             else:
                 tgt_context_list = []
                 
-                # Check each context index given the context size and current input index
-                for context_window in range(tgt_context_size, 0, -1):
+                # Check each context index given the context size and current target index
+                for context_window in range(target_context_size, 0, -1):
                     context_idx = idx - context_window
                     
                     # If context idx is not the left side of the beggining of the inputs
                     if context_idx >= 0:
-                        #Store the context in a list
+                        # Store the context in a list
                         tgt_context_list.append(doc_target[context_idx])
-                    
+                
+                # When there is no context, store </t> in context
                 if len(tgt_context_list) ==0:
-                    new_doc_context.append("</t>")
+                    new_doc_context.append("</t>") # SU: when there is no context still there is contexxt_ids
                     new_doc_target.append(tgt)
-                    
+
+                # When there are contects, concatenate and append to the context list 
                 else:
                     concat_contexts = "</t>".join(tgt_context_list)
-                    #print (concat_contexts)
-                    if tgt_sep : #
-                        #print ("------------")
+                    concat_contexts += "</t>" # After contexts, add sep token
+        
+                    if tgt_sep : 
                         new_doc_context.append(concat_contexts)
-                        print ("new_doc_context1", new_doc_context)
                         new_doc_target.append(tgt)
                         #print ("new_doc_target", new_doc_target)
                     else:
                         new_target = "</t>".join([concat_contexts,tgt])
                         new_doc_target.append(new_target) #
-        
-        new_tgt_contexts.append(new_doc_context) #     
+
+        # Collect the document level target to a list
+        new_tgt_contexts.append(new_doc_context) 
         new_targets.append(new_doc_target)
-        #print ("new_targets1", new_targets[:10])
 
-    new_tgt_contexts = [sent for doc in new_tgt_contexts for sent in doc]
-    print ("new_tgt_contexts[:10]", new_tgt_contexts[:10])
-    context_out  = tokenizer(new_tgt_contexts, max_length=max_length,  truncation=False, padding=True) # Modify False
-    context_ids = context_out['input_ids'] #
-    context_attn = context_out['attention_mask'] #
-    print ("context_ids", context_ids)
-    new_inputs = [sent for doc in new_inputs for sent in doc]  
-    print ("new_inputs[30:40]", new_inputs[30:40])  
+    # Extract sentence level context in a list
+    new_inputs = [sent for doc in new_inputs for sent in doc]
     new_targets = [sent for doc in new_targets for sent in doc] 
-    print ("new_targets[30:40]", new_targets[30:40] )    
+    new_tgt_contexts = [sent for doc in new_tgt_contexts for sent in doc]
+    #context_out  = tokenizer(new_tgt_contexts, max_length=max_length,  truncation=False, padding=True) # Modify False
+    #context_ids = context_out['input_ids'] #
+    #context_attn = context_out['attention_mask'] #
 
+    # Tokenize input and target 
     model_inputs = tokenizer(
-            new_inputs, text_target=new_targets, max_length=max_length, truncation=False, padding=True
+            new_inputs, text_target=new_targets, max_length=max_length, truncation=False, padding = True
         )
 
-    if tgt_context_size>0:
-        ilen = len(context_ids[0])
-        for ic, con in enumerate(context_ids): ####How does this work?"#####
-            assert ilen == len(con), "line is  {0}".format(con)
+    # Tokenize context indipendently
+    if tgt_context_size>0: 
+        #print ("new_tgt_contexts", new_tgt_contexts)#SU
+        context_out  = tokenizer(new_tgt_contexts, max_length=max_length,  truncation=False, padding = True) ### SU　
+        context_ids = context_out['input_ids'] ### SU
+        context_attn = context_out['attention_mask'] ### SU
         
+        # Add tokenized context information on model_inputs
         model_inputs['context_ids']=context_ids
         model_inputs['context_attention_mask']=context_attn
-        print ('model_inputs["context_ids"]', model_inputs["context_ids"])
 
+        print ("\nDecoded tokinized context_ids: ", tokenizer.batch_decode(model_inputs["context_ids"][0:5], skip_special_tokens=True))
+        print ("\nDecoded tokinized context_atten: ", tokenizer.batch_decode(model_inputs['context_attention_mask'][0:5],skip_special_tokens=True))
+       
     if cw_dropout_rate>0:
-        #new_input_ids = []
         for i,inst in enumerate(model_inputs['input_ids']):
             sep_indices = [i for i, x in enumerate(inst) if x == tokenizer.convert_tokens_to_ids("</t>")]
             eos_indices = [i for i, x in enumerate(inst) if x == tokenizer.eos_token_id] #should be only 1
-            # print(sep_indices)
-            # print(eos_indices)
-            # print('----')
+    
             if len(sep_indices)>0:
                 sub = inst[sep_indices[-1]:eos_indices[-1]] # get the indices of the input to be modified (source)
                 sub = list(range(sep_indices[-1],eos_indices[-1]))
@@ -142,8 +182,7 @@ def preprocess_function(src_context_size, tgt_context_size, cw_dropout_rate, tgt
 
         print ("\nDecoded tokinized input-ids: ", tokenizer.batch_decode(model_inputs['input_ids'][30:40], skip_special_tokens=True))
         print ("\nDecoded tokinized labels: ", tokenizer.batch_decode(model_inputs['labels'][30:40], skip_special_tokens=True))
-        #print ("\nDecoded tokinized context_ids: ", tokenizer.batch_decode(model_inputs["context_ids"][0:5], skip_special_tokens=True))
-        #print ("\nDecoded tokinized context_atten: ", tokenizer.batch_decode(model_inputs['context_attention_mask'][30:40],skip_special_tokens=True))
+
     return model_inputs
 
 
